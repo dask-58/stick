b239035ccb38d4ebdc9bf96bf3d648dee146693d
dask-58 <140686560+dask-58@users.noreply.github.com>
2025-12-24 13:48:14 +0530

    v0.1.1 : gh-actions

--- Formula/stick.rb
+++ Formula/stick.rb
@@ -1,28 +1,24 @@
 class Stick < Formula
   desc "Minimal static site generator in Rust"
   homepage "https://github.com/dask-58/stick"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/dask-58/stick/releases/download/v0.1.0/stick-aarch64-apple-darwin.tar.gz"
-      sha256 "3d2aa09dd59036f1dcfb6d8f0f22d60b18f585c77910b7295e3ba07b10178e55"
+      url "https://github.com/dask-58/stick/releases/download/v#{version}/stick-aarch64-apple-darwin.tar.gz"
     end
     on_intel do
-      url "https://github.com/dask-58/stick/releases/download/v0.1.0/stick-x86_64-apple-darwin.tar.gz"
-      sha256 "3d2aa09dd59036f1dcfb6d8f0f22d60b18f585c77910b7295e3ba07b10178e55"
+      url "https://github.com/dask-58/stick/releases/download/v#{version}/stick-x86_64-apple-darwin.tar.gz"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/dask-58/stick/releases/download/v0.1.0/stick-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "3d2aa09dd59036f1dcfb6d8f0f22d60b18f585c77910b7295e3ba07b10178e55"
+      url "https://github.com/dask-58/stick/releases/download/v#{version}/stick-aarch64-unknown-linux-gnu.tar.gz"
     end
     on_intel do
-      url "https://github.com/dask-58/stick/releases/download/v0.1.0/stick-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "3d2aa09dd59036f1dcfb6d8f0f22d60b18f585c77910b7295e3ba07b10178e55"
+      url "https://github.com/dask-58/stick/releases/download/v#{version}/stick-x86_64-unknown-linux-gnu.tar.gz"
     end
   end
 
@@ -31,6 +27,6 @@ class Stick < Formula
   end
 
   test do
-    system "#{bin}/stick", "--version"
+    system bin/"stick", "--version"
   end
 end
